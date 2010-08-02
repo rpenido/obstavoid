a1768d7e74edbe18c0c867e6abc7187c3247b9ef
romulo.penido <romulo.penido@04251b42-f949-0410-84d6-b336a09adc77>
2010-08-02 17:06:56 +0000

    Clone method

--- Mechanisms/Mechanism.cs
+++ Mechanisms/Mechanism.cs
@@ -10,11 +10,14 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Simples.Robotics.Mechanisms
 {
     [Serializable]
-    public class Mechanism
+    public class Mechanism: ICloneable
     {
         
         private List<Joint> jointList;
@@ -36,5 +39,19 @@ namespace Simples.Robotics.Mechanisms
             jointList = new List<Joint>();
             linkList = new List<Link>();
         }
+
+        #region ICloneable Members
+
+        public object Clone()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, this);
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Close();
+            return bf.Deserialize(ms);               
+        }
+
+        #endregion
     }
 }
